f33333e6a4bf4ef532835a94f05f87e8cb1155bf
wilian <wilianuhlmann@gmail.com>
2021-01-27 13:25:49 -0300

    finish application mobile with flutter

---
+++ android/app/src/main/kotlin/uhlmann/com/buscador_gifs/MainActivity.kt
@@ -0,0 +1,6 @@
+package uhlmann.com.buscador_gifs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:buscador_gifs/ui/home_page.dart';
+import 'package:buscador_gifs/ui/gif_page.dart';
+
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    //home: GifPage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
+
+
